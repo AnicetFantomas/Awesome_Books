454733ad085bdb9a478deb488a3930e3b11b6046
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-03-21 10:27:45 +0100

    Added html and js files

---
+++ main.js
@@ -0,0 +1,16 @@
+const titleInput = document.querySelector("#title");
+const AuthorInput = document.querySelector("#author");
+const getForm = document.querySelector("#books-form");
+
+const booksAdded = [];
+
+class book  {
+   constructor (Author, Title) {
+    this.Author = Author;
+    this.Title = Title;
+   } 
+   addBookList () {};
+   removeBook () {};
+};
+
+
